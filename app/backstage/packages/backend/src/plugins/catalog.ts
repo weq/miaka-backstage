60ca4bac6bf2dae211879e10e22dafebada275eb
weq <weq+github@weq.no>
2023-07-24 18:44:19 +0200

    Added github scheduler integration.

--- app/backstage/packages/backend/src/plugins/catalog.ts
+++ app/backstage/packages/backend/src/plugins/catalog.ts
@@ -2,13 +2,33 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+// START https://backstage.io/docs/integrations/github/discovery/
+import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+// END https://backstage.io/docs/integrations/github/discovery/
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
+
+  // START https://backstage.io/docs/integrations/github/discovery/
+  builder.addEntityProvider(
+    GithubEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      // optional: alternatively, use scheduler with schedule defined in app-config.yaml
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 30 },
+        timeout: { minutes: 3 },
+      }),
+      // optional: alternatively, use schedule
+      scheduler: env.scheduler,
+    }),
+  );
+  // END https://backstage.io/docs/integrations/github/discovery/
+
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
 }
+w
